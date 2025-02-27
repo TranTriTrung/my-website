3c1efd1924e0656476caab51532b214fc86d22ae
TranTriTrung <trantritrung.10m@gmail.com>
2025-02-27 20:22:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function login()
    let p = pw.value;
    if ( u == "trung" && p == "1909")
    {
-     window.open("home.html")document.getElementById(compiler).style.display = "block"
+     window.open("home.html")
+     document.getElementById(compiler).style.display = "block"
      }
 }
